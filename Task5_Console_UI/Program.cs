ac4ffa6c88b0a9d9715c6f054999a1e8b9f5f2ba
Sviatoslav <qqfittoppt@gmail.com>
2018-10-08 12:49:12 +0300

    Menu class Small refactoring Stylecop

--- Task5_Console_UI/Program.cs
+++ Task5_Console_UI/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="Program.cs" company="My company">
+//     Copyright (c) My company". All rights reserved.
+// </copyright>
 
 namespace Task5_Console_UI
 {
-    class Program
+    /// <summary>
+    /// Main class of the program
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Entry point of the program
+        /// </summary>
+        /// <param name="args">Args of command line</param>
+        private static void Main(string[] args)
         {
+            NumberConvertersMenu convertersMenu = new NumberConvertersMenu();
+            convertersMenu.ConsoleMenu();
         }
     }
 }
--- Task5_Lib/NumberConverters.cs
+++ Task5_Lib/NumberConverters.cs
@@ -1,72 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="NumberConverters.cs" company="My company">
+//     Copyright (c) My company". All rights reserved.
+// </copyright>
 
 namespace Task5_Lib
 {
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// Class with number converters methods
+    /// </summary>
     public static class NumberConverters
     {
-        private static readonly string[] dozens =
-            { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-        private static readonly string[] units =
-            { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-                "ten", "eleven", "twelve", "thirteen", "fourteen","fifteen",
-                "sixteen","seventeen","eighteen","nineteen"};
-
-        private static string[] Dozens { get => dozens; }
-        private static string[] Units { get => units; }
-        private static string IntPartToString(int n)
+        /// <summary>
+        /// String values of all dozens
+        /// </summary>
+        private static readonly string[] Dozens =
         {
-            if (n < 0 || n > 999)
-            {
-                throw new ArgumentOutOfRangeException();
-            }
+            string.Empty,
+            string.Empty,
+            "twenty",
+            "thirty",
+            "forty",
+            "fifty",
+            "sixty",
+            "seventy",
+            "eighty",
+            "ninety"
+        };
+        
+        /// <summary>
+        /// String values of all units
+        /// </summary>
+        private static readonly string[] Units =
+        {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen"
+        };
 
-            string answer = "";
-
-            if (n != 0)
-            {
-                int hundreds_count = n / 100;
-                int dozens_count = n % 100 / 10;
-                int units_count = n % 10;
-                if (hundreds_count > 0)
-                {
-                    answer += Units[hundreds_count] + " hundred";
-                }
-
-                if (n % 100 >= 1 && n % 100 <= 19)
-                {
-                    answer += " " + Units[n % 100];
-                }
-                else if (dozens_count > 1)
-                {
-                    if (answer.Length != 0)
-                    {
-                        answer += " ";
-                    }
-
-                    answer += Dozens[dozens_count];
-                    if (units_count != 0)
-                    {
-                        answer += " " + Units[units_count];
-                    }
-                }
-            }
-            return answer.Trim();
-        }
+        /// <summary>
+        /// Converts number to string
+        /// </summary>
+        /// <param name="n">Value to convert</param>
+        /// <returns>Converted string</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Throws when n value is less -999999999 or bigger than 999999999</exception>
         public static string IntToString(int n)
         {
             StringBuilder answer = new StringBuilder(100);
             if (n == 0)
             {
-                answer.Append("zero");
+                answer.Append(Units[0]);
             }
-            if (n > 999999999 || n < -999999999)
+            else if (n > 999999999 || n < -999999999)
             {
                 throw new ArgumentOutOfRangeException();
             }
+
             bool wasAppendMade = false;
 
             if (n < 0)
@@ -75,6 +82,7 @@ namespace Task5_Lib
                 answer.Append("minus");
                 wasAppendMade = true;
             }
+
             int millions = n / 1000000;
             n -= millions * 1000000;
             int thousands = n / 1000;
@@ -87,30 +95,74 @@ namespace Task5_Lib
                 {
                     answer.Append(" ");
                 }
+
                 answer.Append(IntPartToString(millions).Trim() + " million");
                 wasAppendMade = true;
             }
 
-            if(thousands != 0)
-            {
-                if(wasAppendMade)
-                {
-                    answer.Append(" ");
-                }
-                answer.Append(IntPartToString(thousands).Trim() + " thousand");
-                wasAppendMade = true;
-            }
-
-            if(hundreds != 0)
+            if (thousands != 0)
             {
                 if (wasAppendMade)
                 {
                     answer.Append(" ");
                 }
+
+                answer.Append(IntPartToString(thousands).Trim() + " thousand");
+                wasAppendMade = true;
+            }
+
+            if (hundreds != 0)
+            {
+                if (wasAppendMade)
+                {
+                    answer.Append(" ");
+                }
+
                 answer.Append(IntPartToString(hundreds).Trim());
             }
 
             return answer.ToString();
+        }
+
+        /// <summary>
+        /// Converts a part of a number to a string
+        /// </summary>
+        /// <param name="n">Number to convert 1-999</param>
+        /// <returns>Converted a part of a number</returns>
+        private static string IntPartToString(int n)
+        {
+            StringBuilder answer = new StringBuilder(100);
+
+            if (n != 0)
+            {
+                int hundreds_count = n / 100;
+                int dozens_count = (n % 100) / 10;
+                int units_count = n % 10;
+                if (hundreds_count > 0)
+                {
+                    answer.Append(Units[hundreds_count] + " hundred");
+                }
+
+                if (n % 100 >= 1 && n % 100 <= 19)
+                {
+                    answer.Append(" " + Units[n % 100]);
+                }
+                else if (dozens_count > 1)
+                {
+                    if (answer.Length != 0)
+                    {
+                        answer.Append(" ");
+                    }
+
+                    answer.Append(Dozens[dozens_count]);
+                    if (units_count != 0)
+                    {
+                        answer.Append(" " + Units[units_count]);
+                    }
+                }
+            }
+
+            return answer.ToString().Trim();
         }
     }
 }
