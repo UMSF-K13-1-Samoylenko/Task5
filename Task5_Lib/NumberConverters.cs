a69d5a40da0932eb95f3656bb002d29c4b7b6d52
Sviatoslav <qqfittoppt@gmail.com>
2018-10-13 13:33:10 +0300

    string[] to Dictionary<int,string> Some changes in algorithm

--- Task5_Lib/NumberConverters.cs
+++ Task5_Lib/NumberConverters.cs
@@ -1,22 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Task5_Lib
 {
     public static class NumberConverters
     {
-        private static readonly string[] dozens =
-            { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-        private static readonly string[] units =
-            { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-                "ten", "eleven", "twelve", "thirteen", "fourteen","fifteen",
-                "sixteen","seventeen","eighteen","nineteen"};
-
-        private static string[] Dozens { get => dozens; }
-        private static string[] Units { get => units; }
+        private static readonly Dictionary<int, string> dozens =
+            new Dictionary<int, string>()
+            {
+                {2, "twenty" },
+                {3, "thirty" },
+                {4, "forty" },
+                {5, "fifty" },
+                {6, "sixty" },
+                {7, "seventy" },
+                {8, "eighty" },
+                {9, "ninety" }
+            };
+        private static readonly Dictionary<int, string> units =
+            new Dictionary<int, string>()
+            {
+                {0, "zero" },
+                {1, "one" },
+                {2, "two" },
+                {3, "three" },
+                {4, "four" },
+                {5, "five" },
+                {6, "six" },
+                {7, "seven" },
+                {8, "eight" },
+                {9, "nine" },
+                {10, "ten" },
+                {11, "eleven" },
+                {12, "twelve" },
+                {13, "thirteen" },
+                {14, "fourteen" },
+                {15, "fifteen" },
+                {16, "sixteen" },
+                {17, "seventeen" },
+                {18, "eighteen" },
+                {19, "nineteen" }
+            };
+        private static readonly Dictionary<int, string> keywords =
+            new Dictionary<int, string>()
+            {
+                {0,string.Empty },
+                {1,"thousand" },
+                {2,"million" }
+            };
         private static string IntPartToString(int n)
         {
             if (n < 0 || n > 999)
@@ -33,12 +65,12 @@ namespace Task5_Lib
                 int units_count = n % 10;
                 if (hundreds_count > 0)
                 {
-                    answer += Units[hundreds_count] + " hundred";
+                    answer += units[hundreds_count] + " hundred";
                 }
 
                 if (n % 100 >= 1 && n % 100 <= 19)
                 {
-                    answer += " " + Units[n % 100];
+                    answer += " " + units[n % 100];
                 }
                 else if (dozens_count > 1)
                 {
@@ -47,15 +79,16 @@ namespace Task5_Lib
                         answer += " ";
                     }
 
-                    answer += Dozens[dozens_count];
+                    answer += dozens[dozens_count];
                     if (units_count != 0)
                     {
-                        answer += " " + Units[units_count];
+                        answer += " " + units[units_count];
                     }
                 }
             }
             return answer.Trim();
         }
+
         public static string IntToString(int n)
         {
             StringBuilder answer = new StringBuilder(100);
@@ -63,54 +96,30 @@ namespace Task5_Lib
             {
                 answer.Append("zero");
             }
-            if (n > 999999999 || n < -999999999)
+            else if (n > 999999999 || n < -999999999)
             {
                 throw new ArgumentOutOfRangeException();
             }
-            bool wasAppendMade = false;
-
+            bool minusAppending = false;
             if (n < 0)
             {
                 n *= -1;
-                answer.Append("minus");
-                wasAppendMade = true;
+                minusAppending = true;
             }
-            int millions = n / 1000000;
-            n -= millions * 1000000;
-            int thousands = n / 1000;
-            n -= thousands * 1000;
-            int hundreds = n;
-           
-            if (millions != 0)
+            for (int i = 0; n != 0; ++i)
             {
-                if (wasAppendMade)
+                int temp = n % 1000;
+                n /= 1000;
+                if (temp != 0)
                 {
-                    answer.Append(" ");
+                    answer.Insert(0,$"{IntPartToString(temp)} {keywords[i]} ");
                 }
-                answer.Append(IntPartToString(millions).Trim() + " million");
-                wasAppendMade = true;
             }
-
-            if(thousands != 0)
+            if(minusAppending)
             {
-                if(wasAppendMade)
-                {
-                    answer.Append(" ");
-                }
-                answer.Append(IntPartToString(thousands).Trim() + " thousand");
-                wasAppendMade = true;
+                answer.Insert(0, $"minus ");
             }
-
-            if(hundreds != 0)
-            {
-                if (wasAppendMade)
-                {
-                    answer.Append(" ");
-                }
-                answer.Append(IntPartToString(hundreds).Trim());
-            }
-
-            return answer.ToString();
+            return answer.ToString().Trim();
         }
     }
 }
